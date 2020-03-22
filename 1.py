9b1011d74c9ca2336a27aa5e5f2a23080783a801
artmikh <61466972+artmikh@users.noreply.github.com>
2020-03-22 19:49:12 +0500

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,3 @@
+my_list = [56, 'hello', False, -40.5, [1, 2, 5], ('red', 'blue', 'green'), {1, 3, 6, 9, 4, 5}, {1: 100, 2: 200, 3: 300}, b'byte', bin(256), None]
+for i in my_list:
+    print(type(i))
